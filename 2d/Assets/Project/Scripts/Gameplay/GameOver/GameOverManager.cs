218713dd8afbdbcb5a6ee745afda3c323a55177b
SLMK <rnjs498913@gmail.com>
2026-05-26 03:34:37 +0900

    게임 오버 화면 수정

--- 2d/Assets/Project/Scripts/Gameplay/GameOver/GameOverManager.cs
+++ 2d/Assets/Project/Scripts/Gameplay/GameOver/GameOverManager.cs
@@ -2,12 +2,9 @@ using UnityEngine;
 using System;
 using System.Collections;
 
-// 역할: 게임 오버 시 패널티 계산 및 플레이 시간 관리
-
 public class GameOverManager : MonoBehaviour
 {
     public static GameOverManager Instance { get; private set; }
-
     public static event Action<GameOverData> OnGameOver;
 
     [Header("사망 애니메이션 대기 시간")]
@@ -19,50 +16,29 @@ public class GameOverManager : MonoBehaviour
 
     void Awake()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
-    void OnEnable()
-    {
-        PlayerStats.OnPlayerDied += HandlePlayerDied;
-    }
-
-    void OnDisable()
-    {
-        PlayerStats.OnPlayerDied -= HandlePlayerDied;
-    }
+    void OnEnable() { PlayerStats.OnPlayerDied += HandlePlayerDied; }
+    void OnDisable() { PlayerStats.OnPlayerDied -= HandlePlayerDied; }
 
     public void StartTimer()
     {
-        if (GameDataManager.Instance.CurrentFloor == 1)
-            playTime = 0f;
-
+        if (GameDataManager.Instance.CurrentFloor == 1) playTime = 0f;
         isGameOver = false;
         isPlaying = true;
     }
 
-    public void StopTimer()
-    {
-        isPlaying = false;
-    }
+    public void StopTimer() { isPlaying = false; }
 
-    void Update()
-    {
-        if (isPlaying)
-            playTime += Time.deltaTime;
-    }
+    void Update() { if (isPlaying) playTime += Time.deltaTime; }
 
     private void HandlePlayerDied()
     {
         if (isGameOver) return;
         isGameOver = true;
-
         isPlaying = false;
         BaseInteractable.IsUIOpen = true;
         StartCoroutine(GameOverRoutine());
@@ -78,7 +54,6 @@ public class GameOverManager : MonoBehaviour
     {
         if (isGameOver) return;
         isGameOver = true;
-
         isPlaying = false;
         BaseInteractable.IsUIOpen = true;
         StartCoroutine(GameOverRoutineImmediate());
@@ -93,31 +68,27 @@ public class GameOverManager : MonoBehaviour
     private GameOverData BuildGameOverData()
     {
         int goldBefore = GameDataManager.Instance.Gold;
+        int floorBefore = GameDataManager.Instance.CurrentFloor;
 
-        // 패널티 전 아이템 목록 복사
         var allItems = new System.Collections.Generic.List<InventoryItem>(Inventory.Instance.Items);
 
         GameDataManager.Instance.ApplyGameOverPenalty();
         Inventory.Instance.ApplyDeathPenalty();
         GameDataManager.Instance.ClearSavedSkills();
 
-        // 패널티 후 살아남은 아이템
         var survivedItems = new System.Collections.Generic.List<InventoryItem>(Inventory.Instance.Items);
 
-        // 사라진 아이템 = 전체 - 살아남은 것
         var lostItems = new System.Collections.Generic.List<InventoryItem>();
         var survivedCopy = new System.Collections.Generic.List<InventoryItem>(survivedItems);
         foreach (var item in allItems)
         {
-            if (survivedCopy.Contains(item))
-                survivedCopy.Remove(item);
-            else
-                lostItems.Add(item);
+            if (survivedCopy.Contains(item)) survivedCopy.Remove(item);
+            else lostItems.Add(item);
         }
 
         return new GameOverData
         {
-            floor = GameDataManager.Instance.CurrentFloor,
+            floor = floorBefore,
             playTime = playTime,
             goldBefore = goldBefore,
             goldAfter = GameDataManager.Instance.Gold,
